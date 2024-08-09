512bbb925344db17910f1ae331311a07981c1294
Adrian <adrian.vidal9589@gmail.com>
2024-08-09 17:48:10 -0600

    Add JSTL taglib and framework for actor and film

--- src/main/java/com/skilldistillery/film/entities/Film.java
+++ src/main/java/com/skilldistillery/film/entities/Film.java
@@ -1,5 +1,151 @@
 package com.skilldistillery.film.entities;
 
+import java.util.List;
+
 public class Film {
+	private int id;
+	private String title;
+	private String description;
+	private Integer releaseYear;
+	private int languageId;
+	private int rentalDuration;
+	private double rentalRate;
+	private Integer length;
+	private double replacementCost;
+	private String rating;
+	private String specialFeatures;
+	private List<Actor> actors;
+
+	public Film() {
+		super();
+	}
+
+	public Film(int id, String title, String description, Integer realeaseYear, int languageId, int rentalDuration,
+			double rentalRate, Integer length, double replacementCost, String rating, String specialFeatures,
+			List<Actor> actors) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.releaseYear = realeaseYear;
+		this.languageId = languageId;
+		this.rentalDuration = rentalDuration;
+		this.rentalRate = rentalRate;
+		this.length = length;
+		this.replacementCost = replacementCost;
+		this.rating = rating;
+		this.specialFeatures = specialFeatures;
+		this.actors = actors;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(Integer realeaseYear) {
+		this.releaseYear = realeaseYear;
+	}
+
+	public int getLanguageId() {
+		return languageId;
+	}
+
+	public void setLanguageId(int languageId) {
+		this.languageId = languageId;
+	}
+
+	public int getRentalDuration() {
+		return rentalDuration;
+	}
+
+	public void setRentalDuration(int rentalDuration) {
+		this.rentalDuration = rentalDuration;
+	}
+
+	public double getRentalRate() {
+		return rentalRate;
+	}
+
+	public void setRentalRate(double rentalRate) {
+		this.rentalRate = rentalRate;
+	}
+
+	public Integer getLength() {
+		return length;
+	}
+
+	public void setLength(Integer length) {
+		this.length = length;
+	}
+
+	public double getReplacementCost() {
+		return replacementCost;
+	}
+
+	public void setReplacementCost(double replacementCost) {
+		this.replacementCost = replacementCost;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	public String getSpecialFeatures() {
+		return specialFeatures;
+	}
+
+	public void setSpecialFeatures(String specialFeatures) {
+		this.specialFeatures = specialFeatures;
+	}
+
+	public List<Actor> getActors() {
+		return actors;
+	}
+
+	public void setActors(List<Actor> actors) {
+		this.actors = actors;
+	}
+
+	@Override
+	public String toString() {
+		return "Film ID: " + id + "\nTitle: " + title + "\nDescription: " + description + "\nRelease Year: "
+				+ releaseYear + "\nLanguage ID: " + languageId + "\nRental Duration: " + rentalDuration
+				+ "\nRental Rate: " + rentalRate + "\nLength: " + length + "\nReplacement Cost: " + replacementCost
+				+ "\nRating: " + rating + "\nSpecial Features: " + specialFeatures + "\nActors: " + actors + "\n\n";
+	}
+
+	
+	public Film[] getFilms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
